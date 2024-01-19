0b36129de2d59508c05ff76dc5bf22400414d064
Pablo Galvez <pablogalvez31@gmail.com>
2024-01-19 23:07:40 +0100

    responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,14 +10,14 @@ const Header = () => {
                 <h1 className='xl:text-7xl lg:text-6xl md:text-5xl sm:text-4xl text-3xl md:text-left text-center font-bold mb-2 text-slate-100 md:typing-effect'>Pablo Gálvez Ortuño</h1>
                 <h2 className='xl:text-2xl lg:text-xl md:text-lg sm:text-base  md:text-left text-center font-medium w-full text-slate-400 mb-3'>Estudiante de <span className='text-yellow-200/[.8]'>Ingeniería Informatica</span>, amante de la programación y el desarrollo.</h2>
                 <div className='flex md:justify-start justify-center text-slate-400'>
-                    <a className='md:mr-4 mx-3 transition duration-200 hover:text-indigo-700' target="_blank" href='https://github.com/Pablo31Monster'>
-                        <Github width="45px" height="45px" style="md:w-11 w-7 md:h-11 h-7"/>
+                    <a className='md:mr-4 mx-2 transition duration-200 hover:text-indigo-700' target="_blank" href='https://github.com/Pablo31Monster'>
+                        <Github width="45px" height="45px" style="md:w-11 w-8 md:h-11 h-8"/>
                     </a>
-                    <a className='md:mr-4 mx-3 transition duration-200 hover:text-blue-500' target="_blank" href='https://www.linkedin.com/in/pablogalvezort'>
-                        <Linkedin width="45px" height="45px" style="md:w-11 w-7 md:h-11 h-7"/>
+                    <a className='md:mr-4 mx-2 transition duration-200 hover:text-blue-500' target="_blank" href='https://www.linkedin.com/in/pablogalvezort'>
+                        <Linkedin width="45px" height="45px" style="md:w-11 w-8 md:h-11 h-8"/>
                     </a>
-                    <a className='md:mr-4 mx-3 transition duration-200 hover:text-red-400' target="_blank" href="/CV.pdf">
-                        <Cv width="45px" height="45px" style="md:w-11 w-7 md:h-11 h-7"/>
+                    <a className='md:mr-4 mx-2 transition duration-200 hover:text-red-400' target="_blank" href="/CV.pdf">
+                        <Cv width="45px" height="45px" style="md:w-11 w-8 md:h-11 h-8"/>
                     </a>
                 </div>
             </div>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const NavBar = ({ children }) => {
     return (
-        <nav className='flex justify-center fixed top-5 left-0 right-0 max-w-[100%]'>
+        <nav className='flex justify-center fixed top-5 left-0 right-0 max-w-[100%] z-50'>
             <ul className='container flex justify-center lg:max-w-lg md:max-w-md sm:max-w-sm max-w-[80%] border rounded-full mx-4 border-slate-800 bg-slate-950'>
                 {children}
             </ul>
