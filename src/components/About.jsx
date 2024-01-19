d280af5c81a28d68ea865cbada8152b67c3cb89d
Pablo Galvez <pablogalvez31@gmail.com>
2024-01-19 23:21:02 +0100

    about me responsive

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -16,49 +16,49 @@ import Git from './icons/Git.jsx';
 const About = () => {
     return (
         <div id='sobre-mi' className='w-full mt-24 pt-10 container flex lg:flex-row flex-col lg:items-normal items-center justify-around m-auto'>
-            <div className='lg:w-1/2 w-3/4 p-12 m-4 border border-slate-800 bg-slate-950 rounded-xl'>
-                <h3 className='text-3xl w-full text-slate-300 pb-1 mb-4 uppercase font-semibold text-center'>Sobre mi</h3>
-                <p className='text-slate-400 font-semibold'>Tengo 20 años y todo el mundo me conoce como Gálvez. Estudio el tercer año del grado en la Universidad de Murcia, España. Empecé a programar en una placa Arduino y he ido evolucionando hasta crear mis propios proyectos personales.</p>
-                <p className='text-slate-400 font-semibold'></p>
+            <div className='lg:w-1/2 w-4/5 sm:p-12 p-6 m-4 border border-slate-800 bg-slate-950 rounded-xl'>
+                <h3 className='sm:text-3xl text-2xl w-full text-slate-300 pb-1 mb-4 uppercase font-semibold text-center'>Sobre mi</h3>
+                <p className='text-slate-400 font-semibold sm:text-base text-sm'>Tengo 20 años y todo el mundo me conoce como Gálvez. Estudio el tercer año del grado en la Universidad de Murcia, España. Empecé a programar en una placa Arduino y he ido evolucionando hasta crear mis propios proyectos personales.</p>
+                <p className='text-slate-400 font-semibold sm:text-base text-sm'></p>
             </div>
-            <div className='lg:w-1/2 w-3/4 p-12 m-4 border border-slate-800 bg-slate-950 rounded-xl'>
-                <h3 className='text-3xl w-full text-slate-300 pb-1 mb-4 uppercase font-semibold text-center'>Habilidades</h3>
+            <div className='lg:w-1/2 w-4/5 sm:p-12 p-6 m-4 border border-slate-800 bg-slate-950 rounded-xl'>
+                <h3 className='sm:text-3xl text-2xl w-full text-slate-300 pb-1 mb-4 uppercase font-semibold text-center'>Habilidades</h3>
                 <div className='grid xl:grid-cols-4 grid-cols-3 gap-4'>
-                    <SkillItem>
-                        <Html width='64px' height='64px' style='m-auto p-1 fill-yellow-200'/>
+                    <SkillItem> 
+                        <Html width='64px' height='64px' style='m-auto p-1 fill-yellow-200 sm:w-16 w-10 sm:h-16 h-10'/>
                     </SkillItem>
                     <SkillItem>
-                        <Css width='64px' height='64px' style='m-auto p-1 fill-yellow-200'/>
+                        <Css width='64px' height='64px' style='m-auto p-1 fill-yellow-200 sm:w-16 w-10 sm:h-16 h-10'/>
                     </SkillItem>
                     <SkillItem>
-                        <JavaScript width='64px' height='64px' style='m-auto p-1 fill-yellow-200'/>
+                        <JavaScript width='64px' height='64px' style='m-auto p-1 fill-yellow-200 sm:w-16 w-10 sm:h-16 h-10'/>
                     </SkillItem>
                     <SkillItem>
-                        <ReactI width='64px' height='64px' style='m-auto p-1 fill-yellow-200'/>
+                        <ReactI width='64px' height='64px' style='m-auto p-1 fill-yellow-200 sm:w-16 w-10 sm:h-16 h-10'/>
                     </SkillItem>
                     <SkillItem>
-                        <Vite width='64px' height='64px' style='m-auto p-1 fill-yellow-200'/>
+                        <Vite width='64px' height='64px' style='m-auto p-1 fill-yellow-200 sm:w-16 w-10 sm:h-16 h-10'/>
                     </SkillItem>
                     <SkillItem>
-                        <TailwindCSS width='64px' height='64px' style='m-auto p-1 fill-yellow-200'/>
+                        <TailwindCSS width='64px' height='64px' style='m-auto p-1 fill-yellow-200 sm:w-16 w-10 sm:h-16 h-10'/>
                     </SkillItem>
                     <SkillItem>
-                        <NodeJS width='64px' height='64px' style='m-auto p-1 fill-yellow-200'/>
+                        <NodeJS width='64px' height='64px' style='m-auto p-1 fill-yellow-200 sm:w-16 w-10 sm:h-16 h-10'/>
                     </SkillItem>
                     <SkillItem>
-                        <ExpressJS width='64px' height='64px' style='m-auto p-1 fill-yellow-200'/>
+                        <ExpressJS width='64px' height='64px' style='m-auto p-1 fill-yellow-200 sm:w-16 w-10 sm:h-16 h-10'/>
                     </SkillItem>
                     <SkillItem>
-                        <Php width='64px' height='64px' style='m-auto p-1 fill-yellow-200'/>
+                        <Php width='64px' height='64px' style='m-auto p-1 fill-yellow-200 sm:w-16 w-10 sm:h-16 h-10'/>
                     </SkillItem>
                     <SkillItem>
-                        <Java width='64px' height='64px' style='m-auto p-1 fill-yellow-200'/>
+                        <Java width='64px' height='64px' style='m-auto p-1 fill-yellow-200 sm:w-16 w-10 sm:h-16 h-10'/>
                     </SkillItem>
                     <SkillItem>
-                        <Git width='64px' height='64px' style='m-auto p-1 fill-yellow-200'/>
+                        <Git width='64px' height='64px' style='m-auto p-1 fill-yellow-200 sm:w-16 w-10 sm:h-16 h-10'/>
                     </SkillItem>
                     <SkillItem>
-                        <MySQL width='64px' height='64px' style='m-auto p-1 fill-yellow-200'/>
+                        <MySQL width='64px' height='64px' style='m-auto p-1 fill-yellow-200 sm:w-16 w-10 sm:h-16 h-10'/>
                     </SkillItem>
                 </div>
             </div>
